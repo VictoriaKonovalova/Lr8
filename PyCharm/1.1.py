b438509b2e64a0805136eb5b13e5a0f86f660b86
VictoriaKonovalova <112749149+VictoriaKonovalova@users.noreply.github.com>
2022-12-20 12:09:40 +0300

    fix

--- PyCharm/1.1.py
+++ PyCharm/1.1.py
@@ -8,10 +8,7 @@ if __name__ == '__main__':
     # Проверить количество элементов кортежа.
     if len(A) != 10:
         print("Неверный размер кортежа", file=sys.stderr)
-    exit(1)
+        exit(1)
     # Найти искомую сумму.
-    s = 0
-    for item in A:
-        if abs(item) < 5:
-            s += item
+    s = sum(a for a in A if abs(a) < 5)
     print(s)
